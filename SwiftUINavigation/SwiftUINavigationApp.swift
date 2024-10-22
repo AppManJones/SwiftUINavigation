1908bc53d2f1563e7078ecff58452cff518b9de6
Joe Rouleau <rouleau.joseph@gmail.com>
2024-10-22 11:51:39 -0400

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Joseph Rouleau on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
